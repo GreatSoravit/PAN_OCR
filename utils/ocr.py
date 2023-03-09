c0326249c3618db230461bf2db4a102ae063c045
Soravit Wetprasit <103516541+GreatSoravit@users.noreply.github.com>
2023-03-09 19:31:22 +0700

    Separate ocr for thai and eng text
    use nameplate type to identify which section need to use eng or thai ocr

--- utils/ocr.py
+++ utils/ocr.py
@@ -3,40 +3,44 @@ from typing import List
 import threading
 
 class OCR(ABC):
-	@abstractmethod
-	def initialize(self):
-		''' Initialize the OCR '''
-		pass
+    @abstractmethod
+    def initialize(self):
+        ''' Initialize the OCR '''
+        pass
 
-	@abstractmethod
-	def ocr_one_image(self, images:List) -> List:
-		''' OCR an image.
-		Input: An array of (area, image)s, opened by PIL and pre-processed
-		Return: An array of (area, message), where the message is from OCR'''
-		pass
+    @abstractmethod
+    def ocr_one_image(self, images:List) -> List:
+        ''' OCR an image.
+        Input: An array of (area, image)s, opened by PIL and pre-processed
+        Return: An array of (area, message), where the message is from OCR'''
+        pass
 
-	def ocr(self, images:List) -> List:
-		'''Sends an opened image to Azure's cognitive services.
-		Input: images (tuple(area, image))
-		Returns the results from Tesseract.'''
-		threads = []
-		threadResults = ["" for i in range(len(images))]
-		threadNum = 0
-		results = []
-		for image in images:
-			t = threading.Thread(target=self.ocr_one_image, args=(image[0], image[1]), kwargs={'threadList':threadResults, 'threadNum':threadNum})
+    def ocr(self, images:List, nameplate:List) -> List:
+        '''Sends an opened image to Azure's cognitive services.
+        Input: images (tuple(area, image))
+        Returns the results from Tesseract.'''
+        threads = []
+        threadResults = ["" for i in range(len(images))]
+        threadNum = 0
+        results = []
+        for idx, image in enumerate(images):
+            #t = threading.Thread(target=self.ocr_one_image, args=(image[0], image[1]), kwargs={'threadList':threadResults, 'threadNum':threadNum})
+            if(nameplate[idx]=='address' or nameplate[idx]=='name' or nameplate[idx]=='religion'):
+                t = threading.Thread(target=self.ocr_one_image, args=(image[0], image[1], 'tha'), kwargs={'threadList':threadResults, 'threadNum':threadNum})
+                #results.append(self.ocr_one_image(image[0], image[1], 'tha'))
+            else:
+                t = threading.Thread(target=self.ocr_one_image, args=(image[0], image[1], 'eng'), kwargs={'threadList':threadResults, 'threadNum':threadNum})
+                #results.append(self.ocr_one_image(image[0], image[1], 'eng'))
+            t.start()
+            threads.append(t)
+            threadNum += 1
+            
+        for t in threads:
+            t.join()
 
-			t.start()
-			threads.append(t)
-			threadNum += 1
+        for i, result in enumerate(threadResults):
+            results.append((images[i][0], result))
+        return results
 
-		for t in threads:
-			t.join()
-		i = 0
-		for result in threadResults:
-			results.append((images[i][0], result))
-			i += 1
-		return results
-
-	def __init__(self):
-		self.initialize()
+    def __init__(self):
+        self.initialize()
